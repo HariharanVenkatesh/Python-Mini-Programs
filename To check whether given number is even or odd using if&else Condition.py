0c9f086ab473b742842e3abc508f454a6f72b8ea
HariharanVenkatesh <harihaniscristo23@gmail.com>
2024-08-05 16:39:18 +0530

    given number is even or odd using if&else Condition

---
+++ To check whether given number is even or odd using if&else Condition.py
@@ -0,0 +1,7 @@
+#Coding Excersice no 3 - To check whether given number is even or odd using if&else Condition
+
+number = int(input("Enter a Number: "))
+if number % 2 == 0:
+    print("This is Even number")
+else:
+    print("This is Odd Number:  ")
