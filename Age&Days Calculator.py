333ea44dae02849661714fa744a661887612fa32
HariharanVenkatesh <harihaniscristo23@gmail.com>
2024-08-05 16:36:07 +0530

    Basic Age&Days Calculator

---
+++ Age&Days Calculator.py
@@ -0,0 +1,8 @@
+#Exercise no 2 - Age and Days Calculator
+
+age=int(input("Enter your age:"))
+years_left=90-age
+days_left = years_left * 365
+months_left = years_left * 12
+weeks_left = years_left * 52
+print(f"You have {days_left} days, {weeks_left} weeks and {months_left} months left.")
