70fe09ad78bd7ea72b6cc24507a3602abf151072
HariharanVenkatesh <harihaniscristo23@gmail.com>
2024-08-05 16:42:19 +0530

    Bmi calculator Using If&else,elif

---
+++ BMI Calculator using if & Else and elif.py
@@ -0,0 +1,17 @@
+
+#Exercise no 4 - BMI Calculator using if & Else and elif
+
+weight = float(input('Enter weight in kg:'))
+height = float(input('Enter height in meter:'))
+bmi = round (weight/height ** 2)
+if bmi <18.5:
+    print (f"Your BMI is {bmi} and you are underweight.")
+elif bmi <25:
+    print (f"Your BMI is {bmi} and you have a normal weight.")
+elif bmi <30:
+    print (f"Your BMI is {bmi} and you are overweight.")
+elif bmi <35:
+    print (f"Your BMI is {bmi} and you are Obese.")
+else:
+    print (f"Your BMI is {bmi} and you are clinically Obese.")
+
