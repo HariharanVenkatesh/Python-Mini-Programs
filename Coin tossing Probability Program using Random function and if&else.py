4bc07297546f330f97a5a44984f6d23dfd379fbc
HariharanVenkatesh <harihaniscristo23@gmail.com>
2024-08-05 16:55:50 +0530

    Program using Random function and if&else

---
+++ Coin tossing Probability Program using Random function and if&else.py
@@ -0,0 +1,11 @@
+#Excercise no 8 - Coin tossing Probability Program using Random function and if&else
+
+import random
+
+side = random.randint(0,1)
+print(side)
+if side == 1:
+    print("Heads")
+else:
+    print("Tails")
+
